defe56d5a38fabcb3392dbc6dd8a58a7aeec2f94
Fred Yang <fredyang001@gmail.com>
2017-04-10 11:15:35 -0700

    Fixes styling issues

--- application/models/Menu.php
+++ application/models/Menu.php
@@ -80,14 +80,14 @@ class Menu extends MY_Model {
 
     // Rules applied to MENU data
 	function rules() {
-		$config = [
-			['field'=>'id', 'label'=>'Menu code', 'rules'=> 'required|integer'],
-			['field'=>'name', 'label'=>'Item name', 'rules'=> 'required'],
-			['field'=>'description', 'label'=>'Item description', 'rules'=> 'required|max_length[256]'],
-			['field'=>'price', 'label'=>'Item price', 'rules'=> 'required|decimal'],
-			['field'=>'picture', 'label'=>'Item picture', 'rules'=> 'required'],
-			['field'=>'category', 'label'=>'Menu category', 'rules'=> 'required']
-		];
-		return $config;
-	}
+        $config = [
+            ['field'=>'id', 'label'=>'Menu code', 'rules'=> 'required|integer'],
+            ['field'=>'name', 'label'=>'Item name', 'rules'=> 'required'],
+            ['field'=>'description', 'label'=>'Item description', 'rules'=> 'required|max_length[256]'],
+            ['field'=>'price', 'label'=>'Item price', 'rules'=> 'required|decimal'],
+            ['field'=>'picture', 'label'=>'Item picture', 'rules'=> 'required'],
+            ['field'=>'category', 'label'=>'Menu category', 'rules'=> 'required']
+        ];
+        return $config;
+    }
 }
